fb6e4512b69ef8e542045aadbfaeb88cfe60e217
Karla Reyes <kmreyes@uc.cl>
2024-08-17 15:23:49 -0400

    configuracion inicial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API App Store')
+    .setDescription('Esta api describe la plataforma de aplicaciones mobile')
+    .setVersion('1.0.0')
+    .addTag('app-store-api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
